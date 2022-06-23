61837b53302c2a3d7a01b97d4e7d8865cbfa6fb8
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 15:35:43 +0200

    Create SpotifyTokenController

---
+++ SpotifySocialMedia/SpotifySocialMedia/Areas/Admin/Controllers/SpotifyTokenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifySocialMedia.Areas.Admin.Controllers
+{
+    public class SpotifyTokenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
