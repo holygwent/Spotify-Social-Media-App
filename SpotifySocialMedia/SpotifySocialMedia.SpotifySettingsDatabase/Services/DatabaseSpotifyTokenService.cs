b4c8ad20d0c39b304e94179f8f0a779606f8ab45
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 15:35:59 +0200

    Create DatabaseSpotifyTokenService

---
+++ SpotifySocialMedia/SpotifySocialMedia.SpotifySettingsDatabase/Services/DatabaseSpotifyTokenService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using SpotifySocialMedia.SpotifyAuthorizationDataDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifySocialMedia.SpotifySettingsDatabase.Services
+{
+    public interface IDatabaseSpotifyTokenService
+    {
+        SpotifyToken GetToken();
+        void AddToken(SpotifyToken token);
+        void DropToken();
+    }
+    public  class DatabaseSpotifyTokenService: IDatabaseSpotifyTokenService
+    {
+        private readonly IMongoCollection<SpotifyToken> _token;
+
+        public DatabaseSpotifyTokenService(IOptions<MongoDatabaseSettings> options)
+        {
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+            _token = mongoClient.GetDatabase(options.Value.DatabaseName)
+                .GetCollection<SpotifyToken>(options.Value.TokenCollectionName);
+        }
+        public SpotifyToken GetToken() => _token.Find(token => true).SingleOrDefault();
+        public void AddToken(SpotifyToken token)
+        {
+            try
+            {
+                _token.InsertOne(token);
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Błąd przesłania tokenu do bazy");
+            }
+        }
+        public void DropToken() => _token.DeleteMany(x => true);
+    }
+}
