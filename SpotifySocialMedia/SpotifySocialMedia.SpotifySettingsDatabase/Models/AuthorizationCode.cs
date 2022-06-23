470f90255b83e12d26805c7c22b18b91f7684158
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 11:46:22 +0200

    Create AuthorizationCode class

---
+++ SpotifySocialMedia/SpotifySocialMedia.SpotifySettingsDatabase/Models/AuthorizationCode.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifySocialMedia.SpotifySettingsDatabase.Models
+{
+    public class AuthorizationCode
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string _id { get; set; }
+        public string Code { get; set; }
+    }
+}
