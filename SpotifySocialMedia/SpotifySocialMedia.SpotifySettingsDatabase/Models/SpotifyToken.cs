d9a297d8924e0824b4cf45a980aaf5033c44b22e
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 15:36:11 +0200

    Create SpotifyToken class

---
+++ SpotifySocialMedia/SpotifySocialMedia.SpotifySettingsDatabase/Models/SpotifyToken.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifySocialMedia.SpotifyAuthorizationDataDatabase.Models
+{
+    public class SpotifyToken
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string scope { get; set; }
+        public int expires_in { get; set; }
+        public string refresh_token { get; set; }
+    }
+}
