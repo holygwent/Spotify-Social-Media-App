6c120b87b5a2628ed6623fb05fdce8e222dd1736
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 21:27:28 +0200

    create SongItemsTrack

---
+++ SpotifySocialMedia/SpotifySocialMedia/Models/SongItems.cs
@@ -0,0 +1,9 @@
+﻿namespace SpotifySocialMedia.Models
+{
+    public class SongItemsTrack
+    {
+        public List<SongItem> songItems { get; set; }
+        public string nextItemsPage { get; set; }
+        public string previosItemsPage { get; set; }
+    }
+}
