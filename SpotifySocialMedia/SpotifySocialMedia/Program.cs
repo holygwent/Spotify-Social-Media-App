24983a841bab08984061bb14101a7eb808aa4c0d
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 21:27:54 +0200

    adjusting

--- SpotifySocialMedia/SpotifySocialMedia/Program.cs
+++ SpotifySocialMedia/SpotifySocialMedia/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SpotifySocialMedia.Areas.Admin.Services;
 using SpotifySocialMedia.Data;
+using SpotifySocialMedia.Services;
 using SpotifySocialMedia.SpotifySettingsDatabase;
 using SpotifySocialMedia.SpotifySettingsDatabase.Services;
 
@@ -20,6 +21,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<MongoDatabaseSettings>(builder.Configuration.GetSection("MongoSettingsDatabase"));
 builder.Services.AddScoped<IAuthorizeService, AuthorizeService>();
 builder.Services.AddScoped<ISpotifyTokenService, SpotifyTokenService>();
+builder.Services.AddScoped<ISearchService , SearchService>();
 //mongo services
 builder.Services.AddSingleton<IDatabaseAuthorizationCodeService, DatabaseAuthorizationCodeService>();
 builder.Services.AddSingleton<IDatabaseSpotifyTokenService, DatabaseSpotifyTokenService>();
@@ -29,6 +31,12 @@ builder.Services.AddHttpClient<ISpotifyTokenService, SpotifyTokenService>(c =>
 {
     c.BaseAddress = new Uri("https://accounts.spotify.com");
     //  c.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
+});
+builder.Services.AddHttpClient<ISearchService, SearchService>(c =>
+{
+    c.BaseAddress = new Uri("https://api.spotify.com/v1/search");
+    c.DefaultRequestHeaders.Add("Accept", "application/.json");
+    
 });
 
 var app = builder.Build();
