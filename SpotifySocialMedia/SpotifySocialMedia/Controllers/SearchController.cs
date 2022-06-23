49e766ed703bff1e5d1366b7ca84719fd90a4871
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 21:27:01 +0200

    create SearchController

---
+++ SpotifySocialMedia/SpotifySocialMedia/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotifySocialMedia.Services;
+
+namespace SpotifySocialMedia.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ISearchService _searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+        public async Task<IActionResult> Index([FromQuery]string search)
+        {
+           var songs =  await _searchService.SearchSong(search);
+            return View(songs);
+        }
+    }
+}
