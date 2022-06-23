d61bea375aa978ae6933ae2cc3001527a78aec4e
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-23 11:46:38 +0200

    Create MongoDatabaseSettings class

---
+++ SpotifySocialMedia/SpotifySocialMedia.SpotifySettingsDatabase/MongoDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifySocialMedia.SpotifySettingsDatabase
+{
+    public  class MongoDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string AuthorizationCodeCollectionName { get; set; }
+        public string TokenCollectionName { get; set; }
+    }
+}
