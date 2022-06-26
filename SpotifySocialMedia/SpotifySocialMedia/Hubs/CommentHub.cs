0871fce411c932be6d026b99df4bab2455c2a76c
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-26 10:26:06 +0200

    create CommentHub

---
+++ SpotifySocialMedia/SpotifySocialMedia/Hubs/CommentHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SpotifySocialMedia.Hubs
+{
+    public class CommentHub : Hub
+    {
+        //public async Task SendMessage(string username,string message)
+        //{
+
+        //  await Clients.All.SendAsync("ReceivedMessage",new  { user=username,message=message});
+        //}
+
+        public async Task JoinGroup(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+        public async Task SendMessageToGroup(string group, string username, string message)
+        {
+            await Clients.Group(group).SendAsync("ReceivedMessage", new { user = username, message = message });
+        }
+        public async Task LeaveGroup(string group)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+        }
+    
+    }
+}
