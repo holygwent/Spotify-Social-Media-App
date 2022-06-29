3608862942e3cb99f8be4ee55d754acdb3bcf486
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-29 16:55:21 +0200

    adding song repository

---
+++ SpotifySocialMedia/SpotifySocialMedia/Services/Repositories/Interfaces/ISongRepository.cs
@@ -0,0 +1,16 @@
+﻿using Database.Entities;
+using SpotifySocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifySocialMedia.Services.Repositories.Interfaces
+{
+    public interface ISongRepository
+    {
+        Task CreateSong(string songId);
+        Task<Song> GetSong(string songId);
+    }
+}
---
+++ SpotifySocialMedia/SpotifySocialMedia/Services/Repositories/SongRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using SpotifySocialMedia.Data;
+using SpotifySocialMedia.Models;
+using SpotifySocialMedia.Services.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifySocialMedia.Services.Repositories
+{
+    internal class SongRepository:ISongRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public SongRepository(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task CreateSong(string songId)
+        {
+           await _dbContext.Songs.AddAsync(new Song { Id = songId });
+           await  _dbContext.SaveChangesAsync();
+        }
+        public async Task<Song> GetSong(string songId)
+        {
+            var song = await _dbContext.Songs
+                .Include(x=>x.Comments)
+                     .ThenInclude(x=>x.Author)
+                .Include(x => x.Comments)
+                     .ThenInclude(x => x.Comments)
+                .FirstOrDefaultAsync(x=>x.Id == songId);
+         
+            return song;
+        }
+    }
+}
