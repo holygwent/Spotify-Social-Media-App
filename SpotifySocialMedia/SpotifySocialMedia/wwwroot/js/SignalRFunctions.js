74f6bab567be2af0e600ca6db08883879422d7ee
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-29 21:09:28 +0200

    update SignalRFunctions

--- SpotifySocialMedia/SpotifySocialMedia/wwwroot/js/SignalRFunctions.js
+++ SpotifySocialMedia/SpotifySocialMedia/wwwroot/js/SignalRFunctions.js
@@ -7,45 +7,59 @@ connection.start().then(() => console.log("Connected"))
 //subscribe to an event method
 connection.on("ReceivedMessage", (data) => {
     console.log(data);
-    var messageEle = document.querySelector("[class=messages]");
-    var liEle = document.createElement("li");
 
-    liEle.innerHTML = `<b>Username: ${data.user} </b> ${data.message}`;
+    let messageEle = document.querySelector("[id=CommentList]");
+    let liEle = document.createElement("li");
+    liEle.classList.add('comment-item');
+
+   liEle.innerHTML = `                       
+                       <div style=\"  line-height:1.7;font-weight:400;\">
+                          <h5 style=\" font-size:20px;\">${data.user}</h5>
+                           <div style=\"color:#ccc !important;font-size:13px;letter-spacing:.1em;text-transform:uppercase;\">${data.shortDate} ${data.shortTime}</div>
+                           <p style=\" margin-top:0;\">${data.message}</p>
+                      </div>`;
+
+
     messageEle.appendChild(liEle);
-    //use data;
+ 
 
 });
 
-//trigger server method
+//trigger server method to send message
 function send() {
-    let username = document.querySelector("[name=username]").value;
+    let username = document.querySelector("[name=AuthorId]").value;
     let message = document.querySelector("[name=msg]").value;
-    let song = document.querySelector("[id=SongId]").getAttribute('alt');
-    if (username != "" && message != "") {
-        connection.invoke("SendMessageToGroup", song, username, message).catch((err) => console.log(err));
+    let group = document.querySelector("[id=SongId]").getAttribute('alt');
+    let songId = document.querySelector("[name=songId]").value;
+
+    if (username != "null" && message != "" && songId != "") {
+        connection.invoke("SendMessageToGroup", group, username, message,songId).catch((err) => console.log(err));
     }
+    message.value = "";
+   
 };
 
-//przy przyciśnięciu zobacz komentarze doda do grupy
+//trigger server method join user to group
 function join(element) {
-    console.log("join element:" + element.getAttribute('alt'))
     connection.invoke("JoinGroup", element.getAttribute('alt')).catch(function (err) {
         return console.error(err.toString());
     });
+
     element.visibility = "hidden";
     element.style.display = "none";
+
     var header = document.getElementById('CommentsHeader');
     var list = document.getElementById('CommentList');
     var commentSong = document.getElementById('CommentSong');
+
     header.style.visibility = "visible";
     list.style.visibility = "visible";
     commentSong.style.visibility = "visible";
 };
-
+////trigger server method leave user from group
 window.onbeforeunload = function () {
 
     let song = document.querySelector("[id=SongId]").getAttribute('alt');
-    console.log('leave'+song);
     connection.invoke("LeaveGroup", song).catch(function (err) {
         return console.error(err.toString());
     });
