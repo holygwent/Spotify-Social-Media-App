15e67f3e6470f9da4bbb21339aaaa472eca5f931
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-29 16:57:20 +0200

    adjustings settings

---
+++ SpotifySocialMedia/Database/Entities/Comment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Entities
+{
+    public class Comment
+    {
+       
+        public string Id { get; set; }
+        [Required]
+        public Song Song { get; set; }
+        [Required]
+        public IdentityUser Author { get; set; }
+        public string Content { get; set; }
+        public Comment? Parent { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public virtual IEnumerable<Comment> Comments { get; set; }
+    }
+}
---
+++ SpotifySocialMedia/Database/Entities/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Entities
+{
+    public class Song
+    {
+        public string Id { get; set; }
+        public virtual IEnumerable<Comment> Comments { get; set; }
+        
+    }
+}
