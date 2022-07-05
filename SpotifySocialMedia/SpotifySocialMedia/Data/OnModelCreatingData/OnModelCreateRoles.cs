839534e436d8e6bdfef311d5e30da706ac65737a
holygwent <39002523+holygwent@users.noreply.github.com>
2022-07-05 18:21:28 +0200

    Create OnModelCreateRoles

---
+++ SpotifySocialMedia/SpotifySocialMedia/Data/OnModelCreatingData/OnModelCreateRoles.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SpotifySocialMedia.Data.OnModelCreatingData
+{
+    public  class OnModelCreateRoles
+    {
+        public static IdentityRole GetRoles()
+        {
+            IdentityRole adminRole = new IdentityRole()
+            {
+                Id = Guid.Parse("7d6f59b9-3f81-4678-9a40-f1d018e711ca").ToString(),
+                Name = "Admin",
+                ConcurrencyStamp = "1",
+                NormalizedName = "ADMIN"
+
+            };
+            return adminRole;
+        }
+    }
+}
