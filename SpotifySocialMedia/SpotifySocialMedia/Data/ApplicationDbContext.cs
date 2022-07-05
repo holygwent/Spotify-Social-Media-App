3d215577aa3a9cccdf0bbfcecec0a9691c7199aa
holygwent <39002523+holygwent@users.noreply.github.com>
2022-07-05 18:20:05 +0200

    OnModelCreating function add admin data to tables

--- SpotifySocialMedia/SpotifySocialMedia/Data/ApplicationDbContext.cs
+++ SpotifySocialMedia/SpotifySocialMedia/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Database.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SpotifySocialMedia.Data.OnModelCreatingData;
 using SpotifySocialMedia.Models;
 
 namespace SpotifySocialMedia.Data
@@ -15,5 +17,25 @@ namespace SpotifySocialMedia.Data
         public DbSet<Song> Songs { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Artist> Artists { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Roles
+            builder.Entity<IdentityRole>(b =>
+            {
+
+                b.HasData(OnModelCreateRoles.GetRoles());
+            });
+            builder.Entity<IdentityUser>(b =>
+            {
+                b.HasData(OnModelCreatingAdmin.CreateAdmin());
+            });
+            builder.Entity<IdentityUserRole<string>>(b =>
+            {
+                b.HasData(OnModelCreatingAddRolesToUsers.AddRolesToUsers());
+            });
+        }
     }
 }
