e201395c5f6931cc8e7efa2225090ecc7774b8bc
holygwent <39002523+holygwent@users.noreply.github.com>
2022-07-05 18:20:44 +0200

    adding migration

---
+++ SpotifySocialMedia/SpotifySocialMedia/Data/Migrations/20220705161642_addingOnModelCreatingAdmin.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifySocialMedia.Data.Migrations
+{
+    public partial class addingOnModelCreatingAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "7d6f59b9-3f81-4678-9a40-f1d018e711ca", "1", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "8931ce67-348b-48b6-96fc-6fc47a74311e", 0, "29c766f0-3d7f-491a-b6ff-e93ada648b9e", "Admin@gmail.com", true, false, null, "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", "AQAAAAEAACcQAAAAEG/us/wwnYl6DJUXfZm626kF3awWVXAVHYZUm4IkEkl/w56tQQNHEUgfoHq0qQKT9g==", "999111222", true, "a1c88747-b10e-47b4-83f1-e67192f670bd", false, "Admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "7d6f59b9-3f81-4678-9a40-f1d018e711ca", "8931ce67-348b-48b6-96fc-6fc47a74311e" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "7d6f59b9-3f81-4678-9a40-f1d018e711ca", "8931ce67-348b-48b6-96fc-6fc47a74311e" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7d6f59b9-3f81-4678-9a40-f1d018e711ca");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8931ce67-348b-48b6-96fc-6fc47a74311e");
+        }
+    }
+}
