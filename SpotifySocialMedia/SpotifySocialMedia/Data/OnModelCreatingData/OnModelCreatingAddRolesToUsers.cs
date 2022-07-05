4b6ed400f0dc93da6865ec0de4276122acd82a8b
holygwent <39002523+holygwent@users.noreply.github.com>
2022-07-05 18:21:16 +0200

    create OnModelCreatingAddRolesToUsers

---
+++ SpotifySocialMedia/SpotifySocialMedia/Data/OnModelCreatingData/OnModelCreatingAddRolesToUsers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SpotifySocialMedia.Data.OnModelCreatingData
+{
+    public class OnModelCreatingAddRolesToUsers
+    {
+        public static IdentityUserRole<string> AddRolesToUsers()
+        {
+            var item = new IdentityUserRole<string>()
+            {
+                RoleId = Guid.Parse("7d6f59b9-3f81-4678-9a40-f1d018e711ca").ToString(),
+                UserId = Guid.Parse("8931ce67-348b-48b6-96fc-6fc47a74311e").ToString()
+            };
+
+            return item;
+        }
+    }
+}
