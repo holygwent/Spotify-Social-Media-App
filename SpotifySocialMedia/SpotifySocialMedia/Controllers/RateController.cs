4663bf195b07a1db0c92db0d734de7b168a2081c
holygwent <39002523+holygwent@users.noreply.github.com>
2022-07-05 11:16:54 +0200

    creating RateController

---
+++ SpotifySocialMedia/SpotifySocialMedia/Controllers/RateController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SpotifySocialMedia.Services.Repositories.Interfaces;
+
+namespace SpotifySocialMedia.Controllers
+{
+    public class RateController : Controller
+    {
+        private readonly IRateRepository _rateRepository;
+
+        public RateController(IRateRepository rateRepository)
+        {
+            _rateRepository = rateRepository;
+        }
+       
+        [Authorize]
+        public IActionResult AddRate(string songId, string user, int starValue)
+        {
+            _rateRepository.Add(songId,user,starValue).Wait();
+            return Ok(_rateRepository.GetAverageRate().Result);
+        }
+    }
+}
