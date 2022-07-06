cde52317aa779fb15c700a7131596705279fd4dd
holygwent <39002523+holygwent@users.noreply.github.com>
2022-07-06 12:40:54 +0200

    adding migration

---
+++ SpotifySocialMedia/SpotifySocialMedia/Data/Migrations/20220706061514_updateSongTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifySocialMedia.Data.Migrations
+{
+    public partial class updateSongTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Songs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8931ce67-348b-48b6-96fc-6fc47a74311e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "603e9fa9-42d3-41d2-9b0c-fb9e40bdf6f3", "AQAAAAEAACcQAAAAECqKuJ3HQO6AcqDARg6Ygo6Z55YtOUbjXsRmYsMh662oob/lFwJ9kIeaSAieTveZYw==", "63797db5-1341-4141-be48-5feeab33f65e" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Songs");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8931ce67-348b-48b6-96fc-6fc47a74311e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "01d446f6-2e93-4e9f-befe-af3cf0d54ae1", "AQAAAAEAACcQAAAAEN34CACcIdn1xT+L1FVPk/hpOwkBsx9ISAJy3BRzKPnDfpkZ0CQeIOt6EkCeVaQAIw==", "8070b73b-3c2b-418b-8d1a-434fa1b975d5" });
+        }
+    }
+}
---
+++ SpotifySocialMedia/SpotifySocialMedia/Data/Migrations/20220706065044_addingNameColumnForArtist.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifySocialMedia.Data.Migrations
+{
+    public partial class addingNameColumnForArtist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Artists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8931ce67-348b-48b6-96fc-6fc47a74311e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5d465562-6ffa-4e4d-9ff8-3809eee2201d", "AQAAAAEAACcQAAAAELgp+8F5uATnkOJURYvT6GpiNLbUUfxRE0+E73aDr+FnQ75WXkK+zbFAxlI6/LLMdA==", "ed90e8c0-951d-4baa-be9c-5a45b5669b26" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Artists");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8931ce67-348b-48b6-96fc-6fc47a74311e",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "603e9fa9-42d3-41d2-9b0c-fb9e40bdf6f3", "AQAAAAEAACcQAAAAECqKuJ3HQO6AcqDARg6Ygo6Z55YtOUbjXsRmYsMh662oob/lFwJ9kIeaSAieTveZYw==", "63797db5-1341-4141-be48-5feeab33f65e" });
+        }
+    }
+}
